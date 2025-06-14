950667033882048ddf0186bd97c20e9302869c81
Zach Podbielniak <z.podbielniak@disroot.org>
2025-06-14 11:42:25 -0400

    feat(config): get xmonad with proper xrandr config on lt-zach

--- .config/xmonad/xmonad.hs
+++ .config/xmonad/xmonad.hs
@@ -23,6 +23,9 @@ import XMonad.Config.Kde
 import XMonad.Config.Xfce
 
 import System.Environment
+import System.Process
+import Data.IntMap (update)
+import Data.List
 
 -- main = do
 --      session <- getEnv "DESKTOP_SESSION"
@@ -49,11 +52,11 @@ main = do
     -- key info
     homeDir <- getEnvVar "HOME" ""
     hostName <- getEnvVar "HOSTNAME" "none"
+    -- xrandrCmd <- getXrandrCmd hostName
 
-    -- set background
-    _ <- spawn $ "feh --bg-fill " 
-        <> homeDir 
-        <> backgroundPicture
+    -- set xrandr 
+    _ <- spawn "xsetup"
+    -- _ <- spawn xrandrCmd
 
     -- spawn compistor
     _ <- spawn $ "picom --config " 
@@ -64,6 +67,11 @@ main = do
     -- spawn xmobar 
     _ <- spawn "xmobar"
 
+    -- set background
+    _ <- spawn $ "sleep 2; feh --bg-fill " 
+        <> homeDir 
+        <> backgroundPicture
+
     -- start xmonad
     xmonad 
         $ ewmhFullscreen 
@@ -72,12 +80,11 @@ main = do
         $ myConfig
 
 
-focusedBorderColor = "#ffffff"
-
 -- configuration, specifically keybinds
 myConfig = def 
     { modMask = mod4Mask -- rebind mod to super
     , layoutHook = myLayout -- use custom layout
+    , focusedBorderColor = "#0663d6"
     }
     -- https://xmonad.github.io/xmonad-docs/xmonad-contrib/XMonad-Util-EZConfig.html
     -- to get keys:
@@ -90,10 +97,10 @@ myConfig = def
     , ("M-S-s", unGrab *> spawn "flatpak --user run org.flameshot.Flameshot gui")
     , ("M-d", spawn "rofi -show drun")
     , ("M-S-d", spawn "rofi -show run")
-    , ("M-S-w", spawn "rofi -show window")
+    , ("M-C-d", spawn "rofi -show window")
     , ("M-S-t", spawn "i3lock -c000000")
     , ("M-S-p", lockAndSuspend)
-    , ("M-S-r", restartXmonad)
+    , ("M-C-r", restartXmonad)
     , ("M-<XF86MonBrightnessUp>", spawn "brightnessctl set 10%+")
     , ("M-<XF86MonBrightnessDown>", spawn "brightnessctl set 10%-")
     , ("M-<XF86KbdBrightnessUp>", spawn "brightnessctl set 10%+")
@@ -104,13 +111,18 @@ myConfig = def
 
 
 -- layout settings
-myLayout = smartSpacing 24 $ tiled ||| Mirror tiled ||| Full ||| threeCol
+-- myLayout = smartSpacing 24 
+-- myLayout = spacingRaw True (Border 0 4 4 4) True (Border 5 4 4 4) True
+myLayout = 
+    spacingRaw False (Border 10 10 10 10) True (Border 10 10 10 10) True
+    $ tiled ||| Mirror tiled ||| Full ||| threeCol
     where 
         threeCol = magnifiercz' 1.3 $ ThreeColMid nmaster delta ratio
         tiled = Tall nmaster delta ratio 
         nmaster = 1 --default number of windows in the master pane 
         ratio = 1/2 -- default proportion of screen occupied by master pane 
         delta = 3/100 -- percent of screen to increment by when resizing panes
+
 
 myXmobarPP :: PP 
 myXmobarPP = def
@@ -142,6 +154,14 @@ myXmobarPP = def
 
 
 
+--        _   _ _ _ _            __                  _   _                 
+--  _   _| |_(_) (_) |_ _   _   / _|_   _ _ __   ___| |_(_) ___  _ __  ___ 
+-- | | | | __| | | | __| | | | | |_| | | | '_ \ / __| __| |/ _ \| '_ \/ __|
+-- | |_| | |_| | | | |_| |_| | |  _| |_| | | | | (__| |_| | (_) | | | \__ \
+--  \__,_|\__|_|_|_|\__|\__, | |_|  \__,_|_| |_|\___|\__|_|\___/|_| |_|___/
+--                      |___/                                              
+
+
 -- lock and suspend
 lockAndSuspend :: X () 
 lockAndSuspend =  do 
@@ -151,8 +171,15 @@ lockAndSuspend =  do
 -- restart xmonad with a recompile
 restartXmonad :: X ()
 restartXmonad = do 
-    spawn "bash -c 'xmonad --recompile; killall -9 picom; killall -9 xmobar; xmonad --restart'"
-    
+    spawn "xmonad --recompile; killall -9 picom; killall -9 xmobar; xmonad --restart"
+ 
+
+--  _          _                    __                  _   _                 
+-- | |__   ___| |_ __   ___ _ __   / _|_   _ _ __   ___| |_(_) ___  _ __  ___ 
+-- | '_ \ / _ \ | '_ \ / _ \ '__| | |_| | | | '_ \ / __| __| |/ _ \| '_ \/ __|
+-- | | | |  __/ | |_) |  __/ |    |  _| |_| | | | | (__| |_| | (_) | | | \__ \
+-- |_| |_|\___|_| .__/ \___|_|    |_|  \__,_|_| |_|\___|\__|_|\___/|_| |_|___/
+--              |_|                                                           
 
 
 getEnvVar :: String -> String -> IO String
@@ -164,7 +191,89 @@ getEnvVar varName defaultValue = do
     --     Nothing -> defaultValue
 
 
+--                   __ _          __ _ _                   _   _   _             
+--   ___ ___  _ __  / _(_) __ _   / _(_) | ___    __ _  ___| |_| |_(_)_ __   __ _ 
+--  / __/ _ \| '_ \| |_| |/ _` | | |_| | |/ _ \  / _` |/ _ \ __| __| | '_ \ / _` |
+-- | (_| (_) | | | |  _| | (_| | |  _| | |  __/ | (_| |  __/ |_| |_| | | | | (_| |
+--  \___\___/|_| |_|_| |_|\__, | |_| |_|_|\___|  \__, |\___|\__|\__|_|_| |_|\__, |
+--                        |___/                  |___/                      |___/ 
+--
+
 getPicomConf :: String -> String 
 getPicomConf hostName 
     | hostName == "lt-zach" = "picom-gpu.conf"
     | otherwise = "picom.conf"
+
+
+
+
+--                           _                      _   _             
+-- __  ___ __ __ _ _ __   __| |_ __   ___  ___  ___| |_(_) ___  _ __  
+-- \ \/ / '__/ _` | '_ \ / _` | '__| / __|/ _ \/ __| __| |/ _ \| '_ \ 
+--  >  <| | | (_| | | | | (_| | |    \__ \  __/ (__| |_| | (_) | | | |
+-- /_/\_\_|  \__,_|_| |_|\__,_|_|    |___/\___|\___|\__|_|\___/|_| |_|
+--                                                                    
+
+
+extractMonitorCount :: [String] -> Int
+extractMonitorCount xrandrOutput = 
+    case xrandrOutput of 
+        (firstLine:_) ->
+            let wordsInLine = words firstLine 
+            in if length wordsInLine >= 2 && head wordsInLine == "Monitors:"
+                then read (wordsInLine !! 1)
+                else 0 
+        [] -> 0
+
+
+getXrandrMonitorList :: IO [String]
+getXrandrMonitorList = do
+    resultRaw <- readProcess "/usr/bin/xrandr" ["--listmonitors"] ""
+    return $ lines resultRaw
+
+
+isDocked :: IO Bool
+isDocked = do
+    monitorList <- getXrandrMonitorList
+    return $ extractMonitorCount monitorList > 1
+
+    
+
+
+getXrandrCmd :: String -> IO String 
+getXrandrCmd "lt-zach" = xrandrLtZach
+getXrandrCmd _ = pure ""
+
+
+xrandrLtZach :: IO String 
+xrandrLtZach = do
+    dockStatus <- isDocked
+    let result = if dockStatus then xrandrLtZachDocked else xrandrLtZachUnDocked
+    return result
+
+
+xrandrLtZachUnDocked :: String 
+xrandrLtZachUnDocked = ""
+
+xrandrLtZachDocked :: String 
+-- xrandrLtZachDocked = unlines -- unlines adds \n
+xrandrLtZachDocked = 
+    intercalate " "
+        [ "xrandr"
+        , "--output eDP-1 --primary --mode 2256x1504 --pos 1461x2880 --rotate normal"
+        , "--output DP-1 --off"
+        , "--output DP-2 --off"
+        , "--output DP-3 --off"
+        , "--output DP-4 --off"
+        , "--output DP-5 --off"
+        , "--output DP-6 --off"
+        , "--output DP-7 --off"
+        , "--output DP-8 --off"
+        , "--output DP-9 --off"
+        , "--output DP-10 --off"
+        , "--output DP-11 --mode 2560x2880 --pos 0x0 --rotate normal"
+        , "--output DP-12 --mode 2560x2880 --pos 2560x0 --rotate normal"
+        , "--output DP-13 --off"
+        ]
+
+
